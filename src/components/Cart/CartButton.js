cb368b06db5caafa9b02ff30cca772fdfc2891ab
Mark Atherton <mark@insightinternet.co.uk>
2022-01-04 17:25:07 +0000

    imported useDispatch from react-redux and stored as a dispatch const

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,7 +1,10 @@
+import { useDispatch } from "react-redux";
 import { uiActions } from "../../store/ui-slice";
 import classes from "./CartButton.module.css";
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+
   const toggleCartHandler = () => {};
 
   return (
