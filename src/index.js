5fdad1a8590ed04ae0a3fa66f6483135153d5960
Mark Atherton <mark@insightinternet.co.uk>
2022-01-04 17:08:04 +0000

    imported Provider from react-redux and wrapped it around <App />

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
