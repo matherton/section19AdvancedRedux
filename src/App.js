ef3a06e51dac133ca259159a7026817983bed4b8
Mark Atherton <mark@insightinternet.co.uk>
2022-01-04 17:36:43 +0000

    imported useSelector in <App /> as this is where the <Cart /> is rendered

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import { useSelector } from "react-redux";
+
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
 
 function App() {
+  useSelector();
   return (
     <Layout>
+      {}
       <Cart />
       <Products />
     </Layout>
