251c005bebd69a6fdacfb1718fae10847d428e42
Mark Atherton <mark@insightinternet.co.uk>
2022-01-04 18:09:42 +0000

    pushing newItem onto the items array if it does not already exist

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -7,5 +7,20 @@ createSlice({
     totalQuantity: 0,
     totalAmount: 0,
   },
-  reducers: {},
+  reducers: {
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      if (!existingItem) {
+        state.items.push({
+          itemId: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title,
+        });
+      }
+    },
+    removeItemFromCart() {},
+  },
 });
