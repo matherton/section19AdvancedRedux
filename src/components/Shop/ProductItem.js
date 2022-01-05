3be1887842ebc10cd3de727881dd58271b91ab29
Mark Atherton <mark@insightinternet.co.uk>
2022-01-05 15:15:10 +0000

    added onClick prop to point to an empty addToCartHandler func

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,8 +1,10 @@
-import Card from '../UI/Card';
-import classes from './ProductItem.module.css';
+import Card from "../UI/Card";
+import classes from "./ProductItem.module.css";
 
 const ProductItem = (props) => {
   const { title, price, description } = props;
+
+  const addToCartHandler = () => {};
 
   return (
     <li className={classes.item}>
@@ -13,7 +15,7 @@ const ProductItem = (props) => {
         </header>
         <p>{description}</p>
         <div className={classes.actions}>
-          <button>Add to Cart</button>
+          <button onClick={addToCartHandler}>Add to Cart</button>
         </div>
       </Card>
     </li>
