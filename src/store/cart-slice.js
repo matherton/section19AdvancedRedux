26f61fba7143246818725cf2cc7aeb58ab61f675
Mark Atherton <mark@insightinternet.co.uk>
2022-01-05 15:53:36 +0000

    added state.totalQuantity to increment by 1 every time the addItemToCart reducer method is called

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -11,6 +11,7 @@ const cartSlice = createSlice({
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
       if (!existingItem) {
         state.items.push({
           itemId: newItem.id,
