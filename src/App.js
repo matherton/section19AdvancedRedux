30b5713169b74fd92cbcdc5aed884aefa65eb123
Mark Atherton <mark@insightinternet.co.uk>
2022-01-07 16:06:01 +0000

    added error check and stored response as const responseData

--- src/App.js
+++ src/App.js
@@ -9,10 +9,17 @@ function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
   useEffect(() => {
-    fetch(
-      "https://react-http-4b88b-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
-      { method: "put", body: JSON.stringify(cart) }
-    );
+    const sendCartData = async () => {
+      const response = await fetch(
+        "https://react-http-4b88b-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+        { method: "put", body: JSON.stringify(cart) }
+      );
+      if (!response.ok) {
+        throw new Error("sending cart data to firebase failed");
+      }
+
+      const responeData = await response.json();
+    };
   }, [cart]);
   return (
     <Layout>
