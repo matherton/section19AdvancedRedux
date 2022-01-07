d5ff7c4da06399141c131cbf1f44159f918fbea0
Mark Atherton <mark@insightinternet.co.uk>
2022-01-07 16:32:10 +0000

    dispatched showNotification with required object key pair values NOTE setup in ui-slice

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 
+import { uiActions } from "./store/ui-slice";
+
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
   useEffect(() => {
     const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "sending...",
+          message: "Sending cart data!",
+        })
+      );
       const response = await fetch(
         "https://react-http-4b88b-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
         { method: "put", body: JSON.stringify(cart) }
