0c7e949819fdf559268dd7b5acc640d524a1cc24
Mark Atherton <mark@insightinternet.co.uk>
2022-01-07 17:05:28 +0000

    rendered <Notification /> conditional using notification as a boolean

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import Notification from "./components/UI/Notification";
 
 import { uiActions } from "./store/ui-slice";
 
@@ -50,10 +51,13 @@ function App() {
     });
   }, [cart, dispatch]);
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification && <Notification />}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
