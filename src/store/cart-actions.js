5425639939c281dc748434766ceac21a38f428a9
Mark Atherton <mark@insightinternet.co.uk>
2022-01-13 11:51:05 +0000

    created cart-actions with fetchCartData function that immeadiately returns dispatch and passes it to fetchData asysc func

---
+++ src/store/cart-actions.js
@@ -0,0 +1,55 @@
+import { get } from "@reduxjs/toolkit/node_modules/immer/dist/internal";
+import { uiActions } from "./ui-slice";
+
+export const fetchCartData = () => {
+  return (dispatch) => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://react-http-4b88b-default-rtdb.europe-west1.firebasedatabase.app/cart.json"
+      );
+    };
+  };
+};
+
+export const sendCardData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "sending...",
+        message: "Sending cart data!",
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://react-http-4b88b-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+        { method: "put", body: JSON.stringify(cart) }
+      );
+
+      if (!response.ok) {
+        throw new Error("sending cart data to firebase failed");
+      }
+    };
+
+    try {
+      await sendRequest();
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "sent",
+          message: "Sent cart data!",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error...",
+          message: "ERROR sending cart data!",
+        })
+      );
+    }
+  };
+};
