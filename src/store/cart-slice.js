d5acbc7cda20715663dbbb249b9198951d7e0116
Mark Atherton <mark@insightinternet.co.uk>
2022-01-13 12:22:53 +0000

    added missing replaceCart reducer

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -8,6 +8,10 @@ const cartSlice = createSlice({
     totalAmount: 0,
   },
   reducers: {
+    replaceCart(state, action) {
+      state.items = action.payload.items;
+      state.totalQuantity = action.payload.totalQuantity;
+    },
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
