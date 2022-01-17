050b8fc53ffbeb5280c867de0612d8caa8111bd2
Mark Atherton <mark@insightinternet.co.uk>
2022-01-17 19:05:45 +0000

    end of lesson 262: ensured items will never be undefined as if it is resolves to a blank array

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -18,7 +18,12 @@ export const fetchCartData = () => {
     };
     try {
       const cartData = await fetchData();
-      dispatch(cartActions.replaceCart(cartData));
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [],
+          totalPrice: cartData.totalPrice,
+        })
+      );
     } catch (error) {
       dispatch(
         uiActions.showNotification({
